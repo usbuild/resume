58aa2f8c07af1487b9513cc89b3e681e9cc3bf30
Usbuild <njuzhangqichao@gmail.com>
2013-03-15 13:02:03 +0800

    fix IE8 compatibility

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var template = document.getElementById("template").innerHTML;
 var xhr = new XMLHttpRequest();
 
 var switchChinese = function () {
-    document.getElementsByClassName("tip")[0].innerHTML = '本简历提供在线 <a href="javascript:switchEnglish();">English</a> 版本， ' +
+    document.getElementById("tip").innerHTML = '本简历提供在线 <a href="javascript:switchEnglish();">English</a> 版本， ' +
         '或者下载PDF (<a href="张启超的简历.pdf">中文</a>, <a href="QichaoZhang_resume.pdf">英文</a>)';
-    document.getElementsByClassName("fork")[0].innerHTML = '<a href="https://github.com/usbuild/resume">在Github上查看源码</a>';
+    document.getElementById("fork").innerHTML = '<a href="https://github.com/usbuild/resume">在Github上查看源码</a>';
     xhr.open("GET", "js/lang.zh_cn.js", true);
     xhr.send();
     xhr.onreadystatechange = function () {
@@ -18,9 +18,9 @@ var switchChinese = function () {
 };
 
 var switchEnglish = function () {
-    document.getElementsByClassName("tip")[0].innerHTML = 'This page is also avaliable in <a href="javascript:switchChinese();">中文</a> , ' +
+    document.getElementById("tip").innerHTML = 'This page is also avaliable in <a href="javascript:switchChinese();">中文</a> , ' +
         'or download it as a PDF (<a href="张启超的简历.pdf">Chinese</a>, <a href="QichaoZhang_resume.pdf#">English</a>)';
-    document.getElementsByClassName("fork")[0].innerHTML = '<a href="https://github.com/usbuild/resume">Fork me on Github</a>';
+    document.getElementById("fork").innerHTML = '<a href="https://github.com/usbuild/resume">Fork me on Github</a>';
     xhr.open("GET", "js/lang.en_us.js", true);
     xhr.send();
     xhr.onreadystatechange = function () {
